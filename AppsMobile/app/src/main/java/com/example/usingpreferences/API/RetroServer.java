befa4f0d1b825cfb8c66303fac80db9ae7c5bcd1
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-19 14:39:21 +0700

    Update RetroServer.java

--- AppsMobile/app/src/main/java/com/example/usingpreferences/API/RetroServer.java
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/API/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String baseURL = "http://172.16.106.200/HufflePuff/DatabaseMobile/";
+    private static final String baseURL = "http://172.16.103.104/HufflePuff/DatabaseMobile/";
                                // http:// ip internetmu /HufflePuff/DatabaseMobile/
 
     private static Retrofit retro;
