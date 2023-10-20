c3b02eb657a8957dd236b6cc3de2faea6a67d719
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-20 18:35:30 +0700

    Create GantiPasswordProfilActivity.java

---
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/GantiPasswordProfilActivity.java
@@ -0,0 +1,16 @@
+package com.example.usingpreferences.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.usingpreferences.R;
+
+public class GantiPasswordProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ganti_password_profil);
+    }
+}
