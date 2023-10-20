ed9af9920d801cc041eec42dfcb82e8b25c14a81
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-20 18:36:35 +0700

    Create PusatBantuanProfil.java

---
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/PusatBantuanProfil.java
@@ -0,0 +1,18 @@
+package com.example.usingpreferences.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.usingpreferences.R;
+
+public class PusatBantuanProfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pusat_bantuan_profil);
+
+
+    }
+}
