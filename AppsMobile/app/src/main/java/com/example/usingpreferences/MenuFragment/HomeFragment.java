3e5fa6eda86881273d50b15066106b0c7a687195
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-21 08:37:36 +0700

    Update HomeFragment.java

--- AppsMobile/app/src/main/java/com/example/usingpreferences/MenuFragment/HomeFragment.java
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/MenuFragment/HomeFragment.java
@@ -4,12 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.ViewPager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,52 +11,42 @@ import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import com.example.usingpreferences.Activity.DetailEventDashboard;
 import com.example.usingpreferences.Activity.NoInduk2;
 import com.example.usingpreferences.Activity.PinjamTempatList;
 import com.example.usingpreferences.Activity.ProfilActivity;
 import com.example.usingpreferences.Adapter.DashboardAdapter;
-import com.example.usingpreferences.Activity.DetailEventDashboard;
 import com.example.usingpreferences.KonfirmMenu.KonfirmasiAwalEvent;
 import com.example.usingpreferences.KonfirmMenu.KonfirmasiKeAdvis;
 import com.example.usingpreferences.R;
 import com.google.android.material.card.MaterialCardView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    TextView tv_namauser,tv_namausertengah;
-    CardView cardviewatas,cardviewtengah,cardizin,cardevent,cardpinjam,cardinduk;
+
+    TextView tv_namauser, tv_namausertengah,textpemberitahuanlayanan,tv_eventTerkini;
+    private Animation fadeIn,fadeIndown,layoutdown;
+    CardView cardviewatas, cardviewtengah, cardizin, cardevent, cardpinjam, cardinduk;
     ScrollView scrollView;
     MaterialCardView card1;
-
+    LinearLayout layoutevent,linearpager;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ViewPager viewPager;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
@@ -80,23 +64,17 @@ public class HomeFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-    private ViewPager viewPager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        // Mengambil referensi ke ViewPager dari layout fragment_home.xml
         viewPager = view.findViewById(R.id.viewPager);
-
-        // Inisialisasi adapter untuk ViewPager
         DashboardAdapter adapter = new DashboardAdapter(getChildFragmentManager());
-
-        // Mengatur adapter ke ViewPager
         viewPager.setAdapter(adapter);
-
-        // Sisipkan kode Anda di sini
-
+        linearpager = view.findViewById(R.id.linear1);
+        tv_eventTerkini = view.findViewById(R.id.tv_eventTerkini);
+        textpemberitahuanlayanan = view.findViewById(R.id.textpemberitahuanlayanan);
         scrollView = view.findViewById(R.id.scrollviewid);
         tv_namauser = view.findViewById(R.id.namauserhome);
         tv_namausertengah = view.findViewById(R.id.namauserhometengah);
@@ -106,15 +84,23 @@ public class HomeFragment extends Fragment {
         cardevent = view.findViewById(R.id.cardevent);
         cardinduk = view.findViewById(R.id.cardinduk);
         cardizin = view.findViewById(R.id.cardizin);
-        card1 =  view.findViewById(R.id.card1);
-
-
+        card1 = view.findViewById(R.id.card1);
+        layoutevent = view.findViewById(R.id.layoutevent);
+        layoutdown = AnimationUtils.loadAnimation(requireContext(), R.anim.layout_in);
+        fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in);
+        fadeIndown = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in_down);
+        tv_eventTerkini.startAnimation(fadeIndown);
+        textpemberitahuanlayanan.startAnimation(fadeIndown);
+        cardpinjam.startAnimation(fadeIndown);
+        cardevent.startAnimation(fadeIndown);
+        cardinduk.startAnimation(fadeIndown);
+        cardizin.startAnimation(fadeIndown);
+        cardviewtengah.startAnimation(fadeIndown);
         card1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), DetailEventDashboard.class));
                 getActivity().overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
-
             }
         });
         cardpinjam.setOnClickListener(new View.OnClickListener() {
@@ -122,7 +108,6 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), PinjamTempatList.class));
                 getActivity().overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
-
             }
         });
         cardevent.setOnClickListener(new View.OnClickListener() {
@@ -130,7 +115,6 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), KonfirmasiAwalEvent.class));
                 getActivity().overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
-
             }
         });
         cardinduk.setOnClickListener(new View.OnClickListener() {
@@ -138,7 +122,6 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), NoInduk2.class));
                 getActivity().overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
-
             }
         });
         cardizin.setOnClickListener(new View.OnClickListener() {
@@ -146,15 +129,8 @@ public class HomeFragment extends Fragment {
             public void onClick(View v) {
                 startActivity(new Intent(getActivity(), KonfirmasiKeAdvis.class));
                 getActivity().overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
-
             }
         });
-
-
-
-
-
-
 
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("prefLogin", Context.MODE_PRIVATE);
         String namaLengkap = sharedPreferences.getString("nama_lengkap", "");
@@ -176,41 +152,30 @@ public class HomeFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), ProfilActivity.class);
                 startActivity(intent);
                 getActivity().overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
-
             }
         });
-
         return view;
     }
-
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
         scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
             @Override
             public void onScrollChanged() {
                 int posisiY = scrollView.getScrollY();
-
                 if (posisiY > 470) {
-                    // Animasi untuk munculkan cardviewatas
-
                     cardviewatas.setVisibility(View.VISIBLE);
-
-
                     cardviewtengah.setVisibility(View.INVISIBLE);
-                    Animation fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in);
                     cardviewtengah.startAnimation(fadeIn);
                 } else if (posisiY < 470) {
-                    // Animasi untuk menghilangkan cardviewatas
                     cardviewatas.setVisibility(View.GONE);
-
-                    // Juga atur cardviewtengah menjadi terlihat
                     cardviewtengah.setVisibility(View.VISIBLE);
+                }
+                if (posisiY < 70) {
+                    layoutevent.startAnimation(fadeIndown);
                 }
             }
         });
     }
-
 }
