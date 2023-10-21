c80e4d7d221b0dfe8b76a47209c06499f174c037
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-21 08:27:14 +0700

    Update PusatBantuanProfil.java

--- AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/PusatBantuanProfil.java
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/PusatBantuanProfil.java
@@ -3,6 +3,8 @@ package com.example.usingpreferences.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.usingpreferences.R;
 
@@ -13,6 +15,17 @@ public class PusatBantuanProfil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pusat_bantuan_profil);
 
+        ImageButton profilback = findViewById(R.id.profilback);
+        profilback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
 
+            }
+        });
+    }   public void onBackPressed(){
+        finish();
+        overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
     }
 }
