b8e7c7a7fa35a10fa741a9a5cbae29c88cd9ad6f
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-21 08:37:39 +0700

    Update EditProfilActivity.java

--- AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/EditProfilActivity.java
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/EditProfilActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import com.example.usingpreferences.R;
@@ -22,7 +23,15 @@ public class EditProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profil);
+        ImageButton profilback = findViewById(R.id.profilback);
+        profilback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
 
+            }
+        });
         tanggallahiredit = findViewById(R.id.tanggaledit);
         tanggallahiredit.setInputType(InputType.TYPE_NULL);
         tanggallahiredit.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +55,8 @@ public class EditProfilActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.jenis_kelamin_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         genderSpinner.setAdapter(adapter);
+    }   public void onBackPressed(){
+        finish();
+        overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
     }
 }
