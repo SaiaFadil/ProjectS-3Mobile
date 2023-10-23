dbbc732d099663c5323077ac1525e967f60e2640
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-23 23:37:48 +0700

    Update LoginActivity.java

--- AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/LoginActivity.java
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/Activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.usingpreferences.Activity;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -42,19 +43,23 @@ public class LoginActivity extends AppCompatActivity {
 
 
 
-    private TextInputEditText mViewUser, mViewPassword;
+    public TextInputEditText mViewUser;
+    public TextInputEditText mViewPassword;
 
     private ProgressDialog progressDialog;
     private Button loginbutton,lupapassword;
     TextView registerbutton;
     private final boolean[] isPasswordVisible = {false};
+
+
+
     @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        SharedPreferences sharedPreferences = getSharedPreferences("prefLogin", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+
         progressDialog = new ProgressDialog(LoginActivity.this);
         progressDialog.setTitle("Loading...");
         progressDialog.setMessage("Sabar");
@@ -120,86 +125,92 @@ public class LoginActivity extends AppCompatActivity {
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mViewUser.setError(null);
-                mViewPassword.setError(null);
-                String email = mViewUser.getText().toString();
-                if (TextUtils.isEmpty(email)) {
-                    mViewUser.setError("Email harus diisi");
-                    mViewUser.requestFocus();
-                } else if (!email.endsWith("@gmail.com")) {
-                    mViewUser.setError("Email tidak valid!!");
-                    mViewUser.requestFocus();
-                } else if(mViewPassword.getText().toString().isEmpty()){
-                    mViewPassword.setError("Password Harus Diisi");
-                    mViewPassword.requestFocus();
-                } else{
-                    APIRequestData ardData = RetroServer.konekRetrofit().create(APIRequestData.class);
-                    Call<ResponseModelUsers> getLoginResponse = ardData.login(mViewUser.getText().toString(), mViewPassword.getText().toString());
-                    getLoginResponse.enqueue(new Callback<ResponseModelUsers>() {
-                        @Override
-                        public void onResponse(Call<ResponseModelUsers> call, Response<ResponseModelUsers> response) {
-
-                            if (response.body().kode == 1){
-                                progressDialog.show();
-                                new Handler().postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        // Tutup ProgressDialog
-                                        progressDialog.dismiss();
-                                        // Simpan semua data pengguna ke SharedPreferences
-                                        ModelUsers user = response.body().getData().get(0);
-                                        editor.putString("id_user", String.valueOf(user.getId_user()));
-                                        editor.putString("nama_lengkap", user.getNama_lengkap());
-                                        editor.putString("no_telpon", user.getNo_telpon());
-                                        editor.putString("tanggal_lahir", user.getTanggal_lahir());
-                                        editor.putString("tempat_lahir", user.getTempat_lahir());
-                                        editor.putString("role", user.getRole());
-                                        editor.putString("email", user.getEmail());
-                                        editor.putString("password", user.getPassword());
-                                        editor.putString("verifikasi", user.getVerifikasi());
-                                        editor.apply();
-                                        Intent pindah = new Intent(LoginActivity.this,MainActivity.class);
-                                        startActivity(pindah);
-                                        overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
-                                    }
-                                }, 2000);
-                                bersihkan();
-
-                            }else if (response.body().kode == 0) {
-                                mViewUser.requestFocus();
-                                Toast.makeText(LoginActivity.this, "Akun Belum Terdaftar", Toast.LENGTH_SHORT).show();
-                            }else if (response.body().kode == 2) {
-                                mViewPassword.requestFocus();
-                                Toast.makeText(LoginActivity.this, "Password Salah!!", Toast.LENGTH_SHORT).show();
-                            }else if(response.body().kode == 3 ){
-                                mViewUser.requestFocus();
-                                Toast.makeText(LoginActivity.this, "Akun Admin Tidak dapat login ke Aplikasi Mobile", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<ResponseModelUsers> call, Throwable t) {
-                            Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                }
+String email = mViewUser.getText().toString();
+String password = mViewPassword.getText().toString();
+                CekLogin(email,password);
 
             }
 
         });
-//Kode agar fullscreen
+
+    }
+
+public void CekLogin(String email,String password){
+    mViewUser.setError(null);
+    mViewPassword.setError(null);
+    if (TextUtils.isEmpty(email)) {
+        mViewUser.setError("Email harus diisi");
+        mViewUser.requestFocus();
+    } else if (!email.endsWith("@gmail.com")) {
+        mViewUser.setError("Email tidak valid!!");
+        mViewUser.requestFocus();
+    } else if(password.isEmpty()){
+        mViewPassword.setError("Password Harus Diisi");
+        mViewPassword.requestFocus();
+    } else{
+        SharedPreferences sharedPreferences = getSharedPreferences("prefLogin", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        APIRequestData ardData = RetroServer.konekRetrofit().create(APIRequestData.class);
+        Call<ResponseModelUsers> getLoginResponse = ardData.login(email,password);
+        getLoginResponse.enqueue(new Callback<ResponseModelUsers>() {
+            @Override
+            public void onResponse(Call<ResponseModelUsers> call, Response<ResponseModelUsers> response) {
+
+                if (response.body().kode == 1){
+                    progressDialog.show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Tutup ProgressDialog
+                            progressDialog.dismiss();
+                            // Simpan semua data pengguna ke SharedPreferences
+                            ModelUsers user = response.body().getData().get(0);
+                            editor.putString("id_user", String.valueOf(user.getId_user()));
+                            editor.putString("nama_lengkap", user.getNama_lengkap());
+                            editor.putString("no_telpon", user.getNo_telpon());
+                            editor.putString("tanggal_lahir", user.getTanggal_lahir());
+                            editor.putString("tempat_lahir", user.getTempat_lahir());
+                            editor.putString("role", user.getRole());
+                            editor.putString("email", user.getEmail());
+                            editor.putString("password", user.getPassword());
+                            editor.putString("verifikasi", user.getVerifikasi());
+                            editor.apply();
+                            Intent pindah = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(pindah);
+                            overridePendingTransition(R.anim.layout_in, R.anim.layout_out);
+                        }
+                    }, 2000);
+                    bersihkan();
+
+                }else if (response.body().kode == 0) {
+                    mViewUser.requestFocus();
+                    Toast.makeText(LoginActivity.this, "Akun Belum Terdaftar", Toast.LENGTH_SHORT).show();
+                }else if (response.body().kode == 2) {
+                    mViewPassword.requestFocus();
+                    Toast.makeText(LoginActivity.this, "Password Salah!!", Toast.LENGTH_SHORT).show();
+                }else if(response.body().kode == 3 ){
+                    mViewUser.requestFocus();
+                    Toast.makeText(LoginActivity.this, "Akun Admin Tidak dapat login ke Aplikasi Mobile", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseModelUsers> call, Throwable t) {
+                Toast.makeText(LoginActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
 
-
+}
 
     private void bersihkan(){
         mViewPassword.setText(null);
         mViewUser.setText(null);
     }
     public void onBackPressed() {
+
     }
 
 }
