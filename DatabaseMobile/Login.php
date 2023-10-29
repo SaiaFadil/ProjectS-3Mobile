3a9f8131ec2d479d945885825334191a42febf01
Fadil <fadillahwahyunugraha@gmail.com>
2023-10-29 09:13:22 +0700

    Dapat Membaca Encrypt Password Dari Database untuk di gunakan login | 29-10-2023

--- DatabaseMobile/Login.php
+++ DatabaseMobile/Login.php
@@ -6,25 +6,32 @@ header("Content-Type: application/json");
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
-    
+
     // Lakukan sanitasi input untuk mencegah SQL injection (gunakan prepared statement jika memungkinkan)
     $email = mysqli_real_escape_string($konek, $email);
-    $password = mysqli_real_escape_string($konek, $password);
-    
-    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password' LIMIT 1";
+
+    $sql = "SELECT * FROM users WHERE email = '$email' LIMIT 1";
     $result = $konek->query($sql);
 
     if ($result->num_rows == 1) {
         $user = $result->fetch_assoc();
-        $role_db = $user['role'];
+        $hashedPasswordFromDatabase = $user['password'];
 
-        if ($role_db == 'masyarakat') {
-            $response["kode"] = 1;
-            $response["pesan"] = "Data Tersedia";
-            $response["data"] = $user;
+        // Memeriksa apakah password yang dimasukkan oleh pengguna cocok dengan hash di database
+        if (password_verify($password, $hashedPasswordFromDatabase)) {
+            $role_db = $user['role'];
+
+            if ($role_db == 'masyarakat') {
+                $response["kode"] = 1;
+                $response["pesan"] = "Data Tersedia";
+                $response["data"] = $user;
+            } else {
+                $response["kode"] = 3;
+                $response["pesan"] = "User Bukan Masyarakat";
+            }
         } else {
-            $response["kode"] = 3;
-            $response["pesan"] = "User Bukan Masyarakat";
+            $response["kode"] = 2;
+            $response["pesan"] = "Email atau Password Salah";
         }
     } else {
         $response["kode"] = 2;
