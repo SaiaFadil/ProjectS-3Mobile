d44f1b832902583ec1de33fce90e7b697c0055b3
Fadil <fadillahwahyunugraha@gmail.com>
2023-11-02 10:47:36 +0700

    Create KategoriSenimanModel.java

---
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/DataModel/KategoriSenimanModel.java
@@ -0,0 +1,22 @@
+package com.example.usingpreferences.DataModel;
+
+public class KategoriSenimanModel {
+    private int id_kategori_seniman;
+    private String nama_kategori;
+
+    public int getId_kategori_seniman() {
+        return id_kategori_seniman;
+    }
+
+    public void setId_kategori_seniman(int id_kategori_seniman) {
+        this.id_kategori_seniman = id_kategori_seniman;
+    }
+
+    public String getNama_kategori() {
+        return nama_kategori;
+    }
+
+    public void setNama_kategori(String nama_kategori) {
+        this.nama_kategori = nama_kategori;
+    }
+}
