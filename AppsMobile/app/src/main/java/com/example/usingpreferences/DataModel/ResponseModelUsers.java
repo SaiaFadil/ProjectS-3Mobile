3f7b4a79733bbb1b833496325eda117aa1f824dd
Fadil <fadillahwahyunugraha@gmail.com>
2023-11-02 10:46:26 +0700

    Update ResponseModelUsers.java

--- AppsMobile/app/src/main/java/com/example/usingpreferences/DataModel/ResponseModelUsers.java
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/DataModel/ResponseModelUsers.java
@@ -3,8 +3,6 @@ package com.example.usingpreferences.DataModel;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import java.util.List;
-
 public class ResponseModelUsers {
 
     @Expose
