a367d741ce01b7efc40d3c4a62857e64be4c2331
Fadil <fadillahwahyunugraha@gmail.com>
2023-11-07 22:31:08 +0700

    database untuk status advis

---
+++ DatabaseMobile/status_advis/edit_advis_diajukan.php
@@ -0,0 +1,43 @@
+<?php
+require('Koneksi.php');
+
+// Menerima data dari aplikasi Android
+$iduser = $_POST['id_user'];
+$nam = $_POST['nama_lengkap'];
+$telp = $_POST['no_telpon'];
+
+
+$cek_iduser = "SELECT * FROM `users` WHERE id_user = '$iduser'";
+$eksekusi_cek = mysqli_query($konek, $cek_iduser);
+$jumlah_cek = mysqli_num_rows($eksekusi_cek);
+
+$response = array();
+if ($jumlah_cek > 0) {
+    
+    $perintah = "UPDATE `users` SET
+     nama_lengkap = '$nam' ,
+     no_telpon = '$telp',
+     jenis_kelamin = '$jeniskel',
+     tanggal_lahir = '$tgllahir',
+     tempat_lahir = '$tmptlahir',
+     email = '$em' where id_user = '$iduser';
+     ";
+
+    $eksekusi = mysqli_query($konek, $perintah);
+
+    if ($eksekusi) {
+        $response["kode"] = 1;
+        $response["pesan"] = "Update Berhasil";
+       
+    } else {
+        $response["kode"] = 2;
+        $response["pesan"] = "Update Gagal";
+    }
+}else{
+    $response["kode"] = 0;
+        $response["pesan"] = "Ada Kesalahan";
+}
+
+echo json_encode($response);
+mysqli_close($konek);
+?>
