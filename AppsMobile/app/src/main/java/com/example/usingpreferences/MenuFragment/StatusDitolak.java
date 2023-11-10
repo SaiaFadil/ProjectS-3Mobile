78d24e38954f5c96c5ff14a5bbb71ba1b51f8e66
Fadil <fadillahwahyunugraha@gmail.com>
2023-11-10 10:27:08 +0700

    Update StatusDitolak.java

--- AppsMobile/app/src/main/java/com/example/usingpreferences/MenuFragment/StatusDitolak.java
+++ AppsMobile/app/src/main/java/com/example/usingpreferences/MenuFragment/StatusDitolak.java
@@ -2,12 +2,11 @@ package com.example.usingpreferences.MenuFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.usingpreferences.Activity.FormAdvisDitolak;
 import com.example.usingpreferences.R;
